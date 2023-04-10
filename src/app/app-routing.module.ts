bdd2083488f04afb2676f7586cf0c2817c3f91d2
Formação: Programação JAVA <36128412022.2@senacrjedu.local>
2023-04-10 08:35:32 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { ListarUsuarioComponent } from './listar-usuario/listar-usuario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { InserirUsuarioComponent } from './inserir-usuario/inserir-usuario.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path: 'sobre', component:SobreComponent},
+  {path:'cadastro', component:CadastroComponent},
+  {path:'inserir-usuario', component:InserirUsuarioComponent},
+  {path:'listar-usuario', component:ListarUsuarioComponent},
+  {path:'editar-usuario', component:EditarUsuarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
